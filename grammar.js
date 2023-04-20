0d22fe3ab68fcd244795d44bfe1a65b942ea1c25
Peter John <peter.john@pyros.sh>
2023-04-21 00:00:46 +0530

    update grammar

--- grammar.js
+++ grammar.js
@@ -1,31 +1,17 @@
-const DEC_DIGITS = token(sep1(/[0-9]+/, /_+/));
-const HEX_DIGITS = token(sep1(/[0-9a-fA-F]+/, /_+/));
-const BIN_DIGITS = token(sep1(/[01]/, /_+/));
 const NEWLINE = /\r?\n/;
 
 module.exports = grammar({
-  name: 'palm',
+  name: "palm",
   // extras: $ => [
   //   $.comment,
   //   /\s+/ // Whitespace
   // ],
   externals: ($) => [$.quoted_content],
-  conflicts: $ => [
-    [$.proc],
-    [$.record],
-  ],
   rules: {
-    source_file: $ => seq(
-      $.module,
-      repeat($.import),
-      repeat(choice(
-        $.record,
-        $.type,
-        $.proc,
-      )),
-    ),
+    source_file: ($) =>
+      seq($.module, repeat($.import), repeat(choice($.type, $.function))),
 
-    module: $ => seq('module', field("module", $.module_name)),
+    module: ($) => seq("module", field("module", $.module_name)),
 
     /* Import statements */
     import: ($) =>
@@ -55,34 +41,25 @@ module.exports = grammar({
 
     /* Declarations */
 
-    record: $ => seq(
-      'record',
-      field('name', $.type_identifier),
-      field('generics', optional($.generic_list)),
-      field('fields', repeat($.type_field)),
-    ),
-
-    type: $ => seq(
-      'type',
-      field('name', $.type_identifier),
-      field('generics', optional($.generic_list)),
-      "=",
-      field('types', pipeSep1($.type_name)),
-    ),
-
-    proc: $ => seq(
-      // 'proc',
-      field('name', $.identifier),
-      field('params', seq('(', optional(commaSep1($.type_field)), ')')),
-      optional(
-        seq(
-          '->',
-          field('return_type', optional($.return_type)),
-        ),
+    type: ($) =>
+      seq(
+        "type",
+        field("name", $.type_identifier),
+        field("generics", optional($.generic_list)),
+        choice(
+          field("record", $.record),
+          field("union", seq("=", optional("|"), sep1($.union, "|")))
+        )
       ),
-      '=',
-      field('body', $._statement_seq),
-    ),
+
+    function: ($) =>
+      seq(
+        "fn",
+        field("name", $.identifier),
+        field("params", seq("(", optional(commaSep1($.type_field)), ")")),
+        optional(seq("->", field("return_type", optional($.return_type)))),
+        field("body", seq("{", $._statement_seq, "}"))
+      ),
 
     /* Shared AST nodes */
 
@@ -120,14 +97,13 @@ module.exports = grammar({
         $.string,
         $.integer,
         $.float,
-        $.record,
         $.identifier,
         $.todo,
         $.panic,
         // $.anonymous_function,
         $.expression_group,
         // $.case,
-        $.negation,
+        $.negation
         // $.record_update,
         // $.field_access,
         // $.function_call
@@ -160,45 +136,29 @@ module.exports = grammar({
     _pattern_binary_expression: ($) =>
       binaryExpr(prec.left, 1, "<>", $._pattern_expression),
 
-
     todo: ($) =>
       seq("todo", optional(seq("(", field("message", $.string), ")"))),
     panic: (_$) => seq("panic"),
 
     /* Types */
-    return_type: $ => seq(
-      field('name', $.type_identifier),
-      field("generics", optional(seq(
-        "(",
-        commaSep1($.identifier),
-        ")",
-      )))
-    ),
+    return_type: ($) =>
+      seq(
+        field("name", $.type_identifier),
+        field("generics", optional(seq("(", commaSep1($.identifier), ")")))
+      ),
 
+    record: ($) => seq(seq("(", commaSep1($.type_field), ")")),
 
-    type_name: $ => seq(
-      field('name', $.type_identifier),
-      optional(seq(
-        "(",
-        choice(
-          $.identifier,
-          commaSep1($.type_field),
-        ),
-        ")",
-      ))
-    ),
+    union: ($) =>
+      seq(
+        field("name", $.type_identifier),
+        optional(seq("(", choice($.identifier, commaSep1($.type_field)), ")"))
+      ),
 
-    type_field: $ => seq(
-      field('name', $.identifier),
-      ':',
-      field('type', $.identifier)
-    ),
+    type_field: ($) =>
+      seq(field("name", $.identifier), ":", field("type", $.identifier)),
 
-    generic_list: $ => seq(
-      '(',
-      commaSep1($.identifier),
-      ')'
-    ),
+    generic_list: ($) => seq("(", commaSep1($.identifier), ")"),
 
     /* Literals */
     float: ($) => /-?[0-9_]+\.[0-9_]*(e-?[0-9_]+)?/,
@@ -214,30 +174,28 @@ module.exports = grammar({
     _decimal: ($) => /[0-9][0-9_]*/,
     _octal: ($) => /0[oO][0-7_]+/,
     _binary: ($) => /0[bB][0-1_]+/,
-    _interpolation: $ => choice(
-      seq("{", $.identifier, "}")
-    ),
+    _interpolation: ($) => choice(seq("{", $.identifier, "}")),
     escape_sequence: ($) => token.immediate(/\\[efnrt\"\\]/),
     // identifier: ($) => $._name,
-    identifier: $ => /[a-zA-Z_][a-zA-Z_0-9]*/,
+    identifier: ($) => /[a-zA-Z_][a-zA-Z_0-9]*/,
     type_identifier: ($) => $._upname,
     _type_annotation: ($) => seq(":", field("type", $.identifier)),
     _discard_name: ($) => /_[_0-9a-z]*/,
     _name: ($) => /[_a-z][_0-9a-z]*/,
     _upname: ($) => /[A-Z][0-9a-zA-Z]*/,
-  }
+  },
 });
 
 function commaSep1(rule) {
-  return sep1(rule, ',')
+  return sep1(rule, ",");
 }
 
 function pipeSep1(rule) {
-  return sep1(rule, '|')
+  return sep1(rule, "|");
 }
 
 function sep1(rule, separator) {
-  return seq(rule, repeat(seq(separator, rule)))
+  return seq(rule, repeat(seq(separator, rule)));
 }
 
 function series_of(rule, separator) {
