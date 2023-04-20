3152e7cf4b18177ccfa00e2c38776434e199e78a
Peter John <peter.john@pyros.sh>
2023-04-20 10:33:04 +0530

    make fns work

--- binding.gyp
+++ binding.gyp
@@ -1,14 +1,15 @@
 {
   "targets": [
     {
-      "target_name": "tree_sitter_pine_binding",
+      "target_name": "tree_sitter_palm_binding",
       "include_dirs": [
         "<!(node -e \"require('nan')\")",
         "src"
       ],
       "sources": [
         "bindings/node/binding.cc",
-        "src/parser.c"
+        "src/parser.c",
+        "src/scanner.c"
       ],
       "cflags_c": [
         "-std=c99",
--- grammar.js
+++ grammar.js
@@ -1,6 +1,7 @@
 const DEC_DIGITS = token(sep1(/[0-9]+/, /_+/));
 const HEX_DIGITS = token(sep1(/[0-9a-fA-F]+/, /_+/));
 const BIN_DIGITS = token(sep1(/[01]/, /_+/));
+const NEWLINE = /\r?\n/;
 
 module.exports = grammar({
   name: 'palm',
@@ -8,6 +9,11 @@ module.exports = grammar({
   //   $.comment,
   //   /\s+/ // Whitespace
   // ],
+  externals: ($) => [$.quoted_content],
+  conflicts: $ => [
+    [$.proc],
+    [$.record],
+  ],
   rules: {
     source_file: $ => seq(
       $.module,
@@ -15,7 +21,7 @@ module.exports = grammar({
       repeat(choice(
         $.record,
         $.type,
-        // $.proc,
+        $.proc,
       )),
     ),
 
@@ -44,6 +50,9 @@ module.exports = grammar({
         )
       ),
 
+    /* Constant statements */
+    // _constant_type_annotation: ($) => seq(":", field("type", $._constant_type)),
+
     /* Declarations */
 
     record: $ => seq(
@@ -61,21 +70,102 @@ module.exports = grammar({
       field('types', pipeSep1($.type_name)),
     ),
 
-    // proc: $ => seq(
-    //   field('name', $.identifier),
-    //   field('params', seq('(', optional(commaSep1($.type_field)), ')')),
-    //   optional(
-    //     seq(
-    //       '->',
-    //       field('return', optional($.return_type)),
-    //     ),
-    //   ),
-    //   '=',
-    //   // field('body', $._block)
-    // ),
+    proc: $ => seq(
+      // 'proc',
+      field('name', $.identifier),
+      field('params', seq('(', optional(commaSep1($.type_field)), ')')),
+      optional(
+        seq(
+          '->',
+          field('return_type', optional($.return_type)),
+        ),
+      ),
+      '=',
+      field('body', $._statement_seq),
+    ),
 
     /* Shared AST nodes */
-    
+
+    // param: $ => seq(
+    //   field('name', $.variable_name),
+    //   ':',
+    //   field('type', $.type)
+    // ),
+
+    /* Expressions */
+
+    _statement_seq: ($) => repeat1($._statement),
+    _statement: ($) => choice($._expression, $.let),
+    _expression: ($) => choice($._expression_unit, $.binary_expression),
+    binary_expression: ($) =>
+      choice(
+        binaryExpr(prec.left, 1, "||", $._expression),
+        binaryExpr(prec.left, 2, "&&", $._expression),
+        binaryExpr(prec.left, 3, "==", $._expression),
+        binaryExpr(prec.left, 3, "!=", $._expression),
+        binaryExpr(prec.left, 4, "<", $._expression),
+        binaryExpr(prec.left, 4, "<=", $._expression),
+        binaryExpr(prec.left, 4, ">", $._expression),
+        binaryExpr(prec.left, 4, ">=", $._expression),
+        binaryExpr(prec.left, 5, "|>", $._expression),
+        binaryExpr(prec.left, 6, "+", $._expression),
+        binaryExpr(prec.left, 6, "-", $._expression),
+        binaryExpr(prec.left, 7, "*", $._expression),
+        binaryExpr(prec.left, 7, "/", $._expression),
+        binaryExpr(prec.left, 7, "%", $._expression),
+        binaryExpr(prec.left, 7, "<>", $._expression)
+      ),
+    _expression_unit: ($) =>
+      choice(
+        $.string,
+        $.integer,
+        $.float,
+        $.record,
+        $.identifier,
+        $.todo,
+        $.panic,
+        // $.anonymous_function,
+        $.expression_group,
+        // $.case,
+        $.negation,
+        // $.record_update,
+        // $.field_access,
+        // $.function_call
+      ),
+    expression_group: ($) => seq("{", $._statement_seq, "}"),
+    negation: ($) => seq("!", $._expression_unit),
+    let: ($) => seq("let", $._assignment),
+    _assignment: ($) =>
+      seq(
+        field("pattern", $._pattern),
+        optional($._type_annotation),
+        "=",
+        field("value", $._expression)
+      ),
+    _pattern: ($) =>
+      seq(
+        $._pattern_expression,
+        optional(field("assign", seq("as", $.identifier)))
+      ),
+    _pattern_expression: ($) =>
+      choice(
+        $.identifier,
+        // $.discard,
+        // $.record_pattern,
+        // $.string,
+        $.integer,
+        $.float,
+        alias($._pattern_binary_expression, $.binary_expression)
+      ),
+    _pattern_binary_expression: ($) =>
+      binaryExpr(prec.left, 1, "<>", $._pattern_expression),
+
+
+    todo: ($) =>
+      seq("todo", optional(seq("(", field("message", $.string), ")"))),
+    panic: (_$) => seq("panic"),
+
+    /* Types */
     return_type: $ => seq(
       field('name', $.type_identifier),
       field("generics", optional(seq(
@@ -103,255 +193,38 @@ module.exports = grammar({
       ':',
       field('type', $.identifier)
     ),
-    identifier: ($) => $._name,
-    type_identifier: ($) => $._upname,
+
     generic_list: $ => seq(
       '(',
       commaSep1($.identifier),
       ')'
     ),
 
+    /* Literals */
+    float: ($) => /-?[0-9_]+\.[0-9_]*(e-?[0-9_]+)?/,
+    integer: ($) =>
+      seq(optional("-"), choice($._hex, $._decimal, $._octal, $._binary)),
+    string: ($) =>
+      seq(
+        '"',
+        repeat(choice($.escape_sequence, $.quoted_content)),
+        token.immediate('"')
+      ),
+    _hex: ($) => /0[xX][0-9a-fA-F_]+/,
+    _decimal: ($) => /[0-9][0-9_]*/,
+    _octal: ($) => /0[oO][0-7_]+/,
+    _binary: ($) => /0[bB][0-1_]+/,
+    _interpolation: $ => choice(
+      seq("{", $.identifier, "}")
+    ),
+    escape_sequence: ($) => token.immediate(/\\[efnrt\"\\]/),
+    // identifier: ($) => $._name,
+    identifier: $ => /[a-zA-Z_][a-zA-Z_0-9]*/,
+    type_identifier: ($) => $._upname,
+    _type_annotation: ($) => seq(":", field("type", $.identifier)),
     _discard_name: ($) => /_[_0-9a-z]*/,
     _name: ($) => /[_a-z][_0-9a-z]*/,
     _upname: ($) => /[A-Z][0-9a-zA-Z]*/,
-
-    // param: $ => seq(
-    //   field('name', $.variable_name),
-    //   ':',
-    //   field('type', $.type)
-    // ),
-
-
-
-    // fun_field: $ => seq(
-    //   'fun',
-    //   field('name', $.identifier),
-    //   field('params', seq('(', optional(commaSep1($.param)), ')')),
-    //   '=>',
-    //   field('returns', optional(commaSep1($.type))),
-    // ),
-
-    // trait_field: $ => choice(
-    //   $.type_field,
-    //   $.fun_field,
-    // ),
-
-    // enum_field: $ => seq($.enum_field_name, '(', commaSep1($._primary_expression), ')'),
-
-    // // Expressions
-
-    // _block: $ => prec(PREC.BLOCK, seq(
-    //   "{",
-    //   // optional($.statements), 
-    //   "}"
-    // )),
-
-    // // assignment: $ => choice(
-    // //   prec.left(PREC.ASSIGNMENT, seq($.directly_assignable_expression, $._assignment_and_operator, $._expression)),
-    // //   prec.left(PREC.ASSIGNMENT, seq($.directly_assignable_expression, "=", $._expression)),
-    // //   // TODO
-    // // ),
-
-    // // // ==========
-    // // // Expressions
-    // // // ==========
-
-    // // _expression: $ => choice(
-    // //   $._unary_expression,
-    // //   $._binary_expression,
-    // //   $._primary_expression
-    // // ),
-
-    // // // Unary expressions
-
-    // // _unary_expression: $ => choice(
-    // //   $.postfix_expression,
-    // //   $.call_expression,
-    // //   $.indexing_expression,
-    // //   $.navigation_expression,
-    // //   $.prefix_expression,
-    // //   $.as_expression,
-    // //   $.spread_expression
-    // // ),
-
-    // // postfix_expression: $ => prec.left(PREC.POSTFIX, seq($._expression, $._postfix_unary_operator)),
-
-    // // call_expression: $ => prec.left(PREC.POSTFIX, seq($._expression, $.call_suffix)),
-
-    // // indexing_expression: $ => prec.left(PREC.POSTFIX, seq($._expression, $.indexing_suffix)),
-
-    // // navigation_expression: $ => prec.left(PREC.POSTFIX, seq($._expression, $.navigation_suffix)),
-
-    // // prefix_expression: $ => prec.right(seq(choice($.annotation, $.label, $._prefix_unary_operator), $._expression)),
-
-    // // as_expression: $ => prec.left(PREC.AS, seq($._expression, $._as_operator, $._type)),
-
-    // // spread_expression: $ => prec.left(PREC.SPREAD, seq("*", $._expression)),
-
-    // // _binary_expression: $ => choice(
-    // //   $.multiplicative_expression,
-    // //   $.additive_expression,
-    // //   $.range_expression,
-    // //   $.infix_expression,
-    // //   $.elvis_expression,
-    // //   $.check_expression,
-    // //   $.comparison_expression,
-    // //   $.equality_expression,
-    // //   $.comparison_expression,
-    // //   $.equality_expression,
-    // //   $.conjunction_expression,
-    // //   $.disjunction_expression
-    // // ),
-
-    // // multiplicative_expression: $ => prec.left(PREC.MULTIPLICATIVE, seq($._expression, $._multiplicative_operator, $._expression)),
-
-    // // additive_expression: $ => prec.left(PREC.ADDITIVE, seq($._expression, $._additive_operator, $._expression)),
-
-    // // range_expression: $ => prec.left(PREC.RANGE, seq($._expression, "..", $._expression)),
-
-    // // infix_expression: $ => prec.left(PREC.INFIX, seq($._expression, $.simple_identifier, $._expression)),
-
-    // // elvis_expression: $ => prec.left(PREC.ELVIS, seq($._expression, "?:", $._expression)),
-
-    // // check_expression: $ => prec.left(PREC.CHECK, seq($._expression, choice(
-    // //   seq($._in_operator, $._expression),
-    // //   seq($._is_operator, $._type)))),
-
-    // // comparison_expression: $ => prec.left(PREC.COMPARISON, seq($._expression, $._comparison_operator, $._expression)),
-
-    // // equality_expression: $ => prec.left(PREC.EQUALITY, seq($._expression, $._equality_operator, $._expression)),
-
-    // // conjunction_expression: $ => prec.left(PREC.CONJUNCTION, seq($._expression, "&&", $._expression)),
-
-    // // disjunction_expression: $ => prec.left(PREC.DISJUNCTION, seq($._expression, "||", $._expression)),
-
-    // //  // Suffixes
-
-    // // indexing_suffix: $ => seq("[", sep1($._expression, ","), "]"),
-
-    // // navigation_suffix: $ => seq(
-    // //    $._member_access_operator,
-    // //    choice(
-    // //      $.simple_identifier,
-    // //      $.parenthesized_expression,
-    // //      "class"
-    // //    )
-    // // ),
-
-    // // call_suffix: $ => prec.left(seq(
-    // //    // this introduces ambiguities with 'less than' for comparisons
-    // //    optional($.type_arguments),
-    // //    choice(
-    // //      seq(optional($.value_arguments), $.annotated_lambda),
-    // //      $.value_arguments
-    // //    )
-    // // )),
-
-    // // variable_declaration: $ => prec.left(PREC.VAR_DECL, seq(
-    // //   // repeat($.annotation), TODO
-    // //   $.simple_identifier,
-    // //   optional(seq(":", $._type))
-    // // )),
-
-    // // function_type: $ => seq(
-    // //   optional(seq($._simple_user_type, ".")), // TODO: Support "real" types
-    // //   $.function_type_parameters,
-    // //   "->",
-    // //   $._type
-    // // ),
-
-    // // function_type_parameters: $ => prec.left(1, seq(
-    // //   "(",
-    // //   optional(sep1(choice($.parameter, $._type), ",")),
-    // //   ")"
-    // // )),
-
-    // // parenthesized_type: $ => seq("(", $._type, ")"),
-
-    // _primary_expression: $ => choice(
-    //   $._literal_constant,
-    //   $._string_literal,
-    // ),
-
-    url: $ => sep1(/[a-zA-Z_][a-zA-Z_0-9]*/, '/'),
-    // module: $ => $.identifier,
-    identifier: $ => /[a-zA-Z_][a-zA-Z_0-9]*/,
-    // definition_name: $ => /[A-Z](([a-z]+[A-Z]?)*)/, // Pascal Case - no digits allowed
-    // variable_name: $ => /[a-z][a-z]*(([A-Z][a-z]+)*[A-Z]?|([a-z]+[A-Z])*|[A-Z])/, // Lower Camel Case - no digits allowed
-    // enum_field_name: $ => /[A-Z_][A-Z_0-9]*/,
-
-    // // Literals
-    // boolean_literal: $ => choice("true", "false"),
-    // integer_literal: $ => token(seq(optional(/[1-9]/), DEC_DIGITS)),
-    // hex_literal: $ => token(seq("0", /[xX]/, HEX_DIGITS)),
-    // bin_literal: $ => token(seq("0", /[bB]/, BIN_DIGITS)),
-    // float_literal: $ => token(choice(
-    //   seq(
-    //     seq(optional(DEC_DIGITS), ".", DEC_DIGITS),
-    //     optional(/[fF]/)
-    //   ),
-    //   seq(DEC_DIGITS, /[fF]/)
-    // )),
-    // character_literal: $ => seq(
-    //   "'",
-    //   choice($.character_escape_seq, /[^\n\r'\\]/),
-    //   "'"
-    // ),
-    // character_escape_seq: $ => choice(
-    //   $._uni_character_literal,
-    //   $._escaped_identifier
-    // ),
-    // _uni_character_literal: $ => seq(
-    //   "\\u",
-    //   /[0-9a-fA-F]{4}/
-    // ),
-    // _escaped_identifier: $ => /\\[tbrn'"\\$]/,
-
-    // _string_literal: $ => choice(
-    //   $.line_string_literal,
-    //   $.multi_line_string_literal
-    // ),
-
-    // line_string_literal: $ => seq('"', repeat(choice($._line_string_content, $._interpolation)), '"'),
-
-    // multi_line_string_literal: $ => seq(
-    //   '`',
-    //   repeat(choice(
-    //     $._multi_line_string_content,
-    //     $._interpolation
-    //   )),
-    //   '`'
-    // ),
-
-    // _line_string_content: $ => choice(
-    //   $._line_str_text,
-    //   $.character_escape_seq
-    // ),
-
-    // _multi_line_string_content: $ => choice($._multi_line_str_text, '"'),
-
-    // _line_str_text: $ => /[^\\"$]+/,
-
-    // _multi_line_str_text: $ => /[^"$]+/,
-
-    // _interpolation: $ => choice(
-    //   // seq("${", alias($._expression, $.interpolated_expression), "}"),
-    //   seq("$", alias($.identifier, $.interpolated_identifier))
-    // ),
-
-    // _literal_constant: $ => choice(
-    //   $.boolean_literal,
-    //   $.integer_literal,
-    //   $.hex_literal,
-    //   $.bin_literal,
-    //   $.character_literal,
-    //   $.float_literal,
-    //   "null",
-    // ),
-
-    // comment: $ => token(prec(PREC.COMMENT, choice(
-    //   seq("#", /.*/),
-    // ))),
   }
 });
 
@@ -369,4 +242,13 @@ function sep1(rule, separator) {
 
 function series_of(rule, separator) {
   return seq(rule, repeat(seq(separator, rule)), optional(separator));
+}
+
+// A binary expression with a left-hand side, infix operator, and then right-hand-side
+// https://github.com/elixir-lang/tree-sitter-elixir/blob/de20391afe5cb03ef1e8a8e43167e7b58cc52869/grammar.js#L850-L859
+function binaryExpr(assoc, precedence, operator, expr) {
+  return assoc(
+    precedence,
+    seq(field("left", expr), field("operator", operator), field("right", expr))
+  );
 }
